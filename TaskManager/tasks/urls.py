e1bdb854571f53cb952dbab12d7ee41c35079900
Dimitris <jim600v@gmail.com>
2024-01-18 22:41:25 +0200

    create views and urls

--- TaskManager/tasks/urls.py
+++ TaskManager/tasks/urls.py
@@ -4,6 +4,11 @@ from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
-    path('token/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
-     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh'),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name ='token-obtain-pair'),
+    path('users/', views.UsersListCreateView.as_view(), name ='users-list-create'),
+    path('projects/', views.ProjectsListCreateView.as_view(), name ='projects-list-create'),
+    path('projects/<int:pk>', views.ProjectsRetrieveUpdateDestroyView.as_view(), name ='project-detail'),
+    path('tasks/', views.TasksListCreateView.as_view(), name ='tasks-list-create'),
+    path('messages', views.MessagesListCreateView.as_view(), name ='messages-list-create'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token-refresh'),
 ]
--- TaskManager/tasks/views.py
+++ TaskManager/tasks/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import Users, Projects, TaskStatus, Tasks, Messages
+from .serializers import UsersSerializer, ProjectsSerializer, TaskStatusSerializer, TasksSerializer, MessageSerializer
 
 # Create your views here.
+
+
+class UsersListCreateView(generics.ListCreateAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+    
+
+class ProjectsListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Projects.objects.all()
+    serializer_class = ProjectsSerializer
+
+
+class ProjectsRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Projects.objects.all()
+    serializer_class = ProjectsSerializer
+
+
+class  TasksListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Tasks.objects.all()
+    serializer_class = TasksSerializer
+
+
+class  TaskStatusListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = TaskStatus.objects.all()
+    serializer_class = TaskStatusSerializer
+
+
+class MessagesListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Messages.objects.all()
+    serializer_class = MessageSerializer
+    
+    
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
